238cba8edeb99a1029619c72a9db34c30b86b9ea
Justin Gao <62669951+encyc@users.noreply.github.com>
2023-12-15 18:13:01 +0800

    update

---
+++ dataprocessing.py
@@ -0,0 +1,14 @@
+# 在单独的文件 dataprocessing.py 中创建数据处理模块
+class DataProcessingModule:
+    def __init__(self, data):
+        self.data = data
+
+    def missing_cal(self):
+        # 实现计算每个变量的缺失率
+        pass
+
+    def plot_missing_var(self):
+        # 实现所有变量缺失值分布图
+        pass
+
+    # 其他数据处理功能...
---
+++ yihui.py
@@ -0,0 +1,36 @@
+# 在 main.py 或者 __init__.py 中创建主类 Yihui
+from eda_module import EDAModule
+import pandas as pd
+
+class Yihui:
+    def __init__(self, data):
+        self.data = data
+        self.categorical_vars = self.get_categorical_variables()
+        self.numeric_vars = self.get_numeric_variables()
+        self.eda_module = EDAModule(data)
+        # 其他模块的初始化...
+
+    def get_categorical_variables(self):
+        # 提取字符型变量的名字并返回一个list
+        return list(self.data.select_dtypes(include='object').columns)
+
+    def get_numeric_variables(self):
+        # 提取数值型变量的名字并返回一个list
+        return list(self.data.select_dtypes(exclude='object').columns)
+
+
+# 在主程序中使用 Yihui 类
+if __name__ == "__main__":
+    # 假设 data 是你的数据
+    with open("data/data.csv", "r") as f:
+        data = pd.read_csv(f)
+
+    # 创建 Yihui 类的实例
+    yihui_project = Yihui(data)
+
+    # 直接访问 Yihui 类的属性获取字符型和数值型变量的名字
+    categorical_vars_list = yihui_project.categorical_vars
+    numeric_vars_list = yihui_project.numeric_vars
+
+    print("Categorical Variables:", categorical_vars_list)
+    print("Numeric Variables:", numeric_vars_list)
