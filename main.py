e6f06de29173ea80c876f05937cea479262491ec
Justin <atomyuangao@gmail.com>
2023-12-26 10:53:48 +0800

    update

--- main.py
+++ main.py
@@ -1,81 +1,45 @@
+# 在 main.py 或者 __init__.py 中创建主类 Yihui
+import warnings
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-# import pandas_profiling
 
-# class DF():
-#     def __init__(self):
-#         self.dataframe = None
-#         self.columns = self.dataframe.columns
-#         self.columns_type = self.dataframe.dtypes
-#         self.target = None
-#
-#     def get_dataframe(self, file_path):
-#         # 根据文件扩展名选择合适的读取函数
-#         if file_path.endswith('.csv'):
-#             df = pd.read_csv(file_path)
-#         elif file_path.endswith('.xlsx') or file_path.endswith('.xls'):
-#             df = pd.read_excel(file_path)
-#         elif file_path.endswith('.txt'):
-#             df = pd.read_csv(file_path, delimiter='\t')
-#         elif file_path.endswith('.json'):
-#             df = pd.read_json(file_path)
-#         else:
-#             raise ValueError("No support file; 不支持的文件格式")
-#         return df
-
-class EDA(object):
-    def __init__(self):
-        self.dataframe = None
-        self.columns = self.dataframe.columns
-        self.target = None
-        self.columns_type = self.dataframe.dtypes
-
-    # def auto_eda(self):
-    #     profile = pandas_profiling.ProfileReport(self.dataframe)
-    #     profile.to_file('auto_eda.html')
-
-    def manual_eda(self):
-        pass
-    def cate_var_distribution(self,column_name):
-        self.dataframe[column_name].value_counts().plot(kind='bar')
-
-    def num_var_distribution(self,column_name):
-        self.dataframe[column_name].plot(kind='hist')
+from yihui import Yihui
 
 
-    def plot_cate_var(self, col_list, hspace=0.4, wspace=0.4, plt_size=None, plt_num=None, x=None, y=None):
-        """
-        df:数据集
-        col_list:变量list集合
-        hspace :子图之间的间隔(y轴方向)
-        wspace :子图之间的间隔(x轴方向)
-        plt_size :图纸的尺寸
-        plt_num :子图的数量
-        x :子图矩阵中一行子图的数量
-        y :子图矩阵中一列子图的数量
 
-        return :变量的分布图（柱状图形式）
-        """
-        plt.figure(figsize=plt_size)
-        plt.subplots_adjust(hspace=hspace, wspace=wspace)
-        plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-        plt.rcParams['axes.unicode_minus'] = False
-        for i, col in zip(range(1, plt_num + 1, 1), col_list):
-            plt.subplot(x, y, i)
-            plt.title(col)
-            sns.countplot(data=df, y=col)
-            plt.ylabel('')
-        return plt.show()
+# 在主程序中使用 Yihui 类
+if __name__ == "__main__":
+    # ban FutureWarning
+    warnings.filterwarnings('ignore')
+
+    # generate data.csv
+    with open("data.csv", "r") as f:
+        data = pd.read_csv(f)
+    data['customer_no'] = str(data['customer_no'])
+
+    # loading Titanic dataset
+    # data = sns.load_dataset('titanic')
+    # data.head()
+
+    # Create Yihui Class
+    yihui_project = Yihui(data,'dlq_flag')
+    print(yihui_project.data.head())
+
+    print("Categorical Variables:", yihui_project.categorical_vars)
+    print("Numeric Variables:", yihui_project.numeric_vars)
+
+    # 直接访问 Yihui 类的属性获取字符型和数值型变量的名字
+    categorical_vars_list = yihui_project.categorical_vars
+    numeric_vars_list = yihui_project.numeric_vars
+
+    # yihui_project.eda_module.auto_eda_profiling()
+
+    # yihui_project.eda_module.plot_num_col(numeric_vars_list,plt_type='hist',hspace=0.4,wspace=0.4,plt_size=(100,100),plt_num=100,x=10,y=10)
+    # yihui_project.eda_module.plot_num_col(numeric_vars_list,plt_type='box',hspace=0.4,wspace=0.4,plt_size=(100,100),plt_num=100,x=10,y=10)
 
 
-# In[]:
-df = pd.read_excel(r'D:\pythonProject\zhizu-work\modeling\同盾多头模型\data\tongdun_duotou_testdataset.xlsx')
-print(df.head())
 
-yihui = EDA()
-yihui.dataframe = df
-yihui.target = 'dlq_flag'
-print(yihui.columns)
-print(yihui.columns_type)
+    # eda_module.plot_default_num
+    # yihui_project.eda_module.plot_default_num(numeric_vars_list,hspace=0.4,wspace=0.4,q=10,plt_size=(100,100),plt_num=100,x=10,y=10)
+
+    # eda_module.plot_default_cate
+    # yihui_project.eda_module.plot_default_cate(categorical_vars_list,hspace=0.4,wspace=0.4,plt_size=(10,10),plt_num=1,x=1,y=1)
