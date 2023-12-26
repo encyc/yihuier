a6600d69efda9ccdb266796c2d09d55756cbf572
Justin <atomyuangao@gmail.com>
2023-12-26 14:37:43 +0800

    update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages
 
 VERSION = '0.1.2'
 
-with open("README.md", "r") as fh:
-    long_description = fh.read
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
 
 setup(
     name='Yihui',  # package name
@@ -19,7 +19,12 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     zip_safe=False,
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
 
 )
 
