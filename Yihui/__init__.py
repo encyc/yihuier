d12ed3e1d6541be51fd3ab0f4f5e6ec12a62c8d3
Justin <atomyuangao@gmail.com>
2023-12-27 00:07:02 +0800

    update

--- Yihui/__init__.py
+++ Yihui/__init__.py
@@ -49,22 +49,28 @@ if __name__ == "__main__":
     # yihui_project.eda_module.plot_default_cate(categorical_vars_list,plt_size=(10,10),plt_num=1,x=1,y=1)
     #
     #
-    # ### data processing 阶段
-    #
-    # # 所有变量缺失值分布图
-    # print(yihui_project.dp_module.plot_bar_missing_var())
-    #
-    # # 使用 '0','median','class','rf'
-    # yihui_project.data = yihui_project.dp_module.fillna_num_var(numeric_vars_list, fill_type='0')
-    #
-    # yihui_project.data = yihui_project.dp_module.fillna_cate_var(categorical_vars_list, fill_type='class', fill_str='missing')
-    # yihui_project.data = yihui_project.dp_module.fillna_cate_var(categorical_vars_list, fill_type='mode')
-    #
-    # # 缺失值剔除
-    # yihui_project.data = yihui_project.dp_module.delete_missing_var()
-    # yihui_project.data = yihui_project.dp_module.delete_missing_obs(threshold=0.2)
-    #
-    # # 常变量/同值化处理
-    # yihui_project.dp_module.const_delete(threshold=0.9)
+    ### data processing 阶段
+
+    # 所有变量缺失值分布图
+    print(yihui_project.dp_module.plot_bar_missing_var())
+
+    # 使用 '0','median','class','rf'
+    yihui_project.data = yihui_project.dp_module.fillna_num_var(numeric_vars_list, fill_type='0')
+
+    yihui_project.data = yihui_project.dp_module.fillna_cate_var(categorical_vars_list, fill_type='class', fill_str='missing')
+    yihui_project.data = yihui_project.dp_module.fillna_cate_var(categorical_vars_list, fill_type='mode')
+
+    # 缺失值剔除
+    yihui_project.data = yihui_project.dp_module.delete_missing_var(threshold=0.2)
+    yihui_project.data = yihui_project.dp_module.delete_missing_obs(threshold=5)
+
+    # 常变量/同值化处理
+    yihui_project.dp_module.const_delete(threshold=0.9)
+
+    # 再检查一下缺失值
+    print(yihui_project.dp_module.plot_bar_missing_var())
 
 
+    # cluster 阶段
+
+    yihui_project.cluster_module.cluster_AffinityPropagation(['v3','v5'])
