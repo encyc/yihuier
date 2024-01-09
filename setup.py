7474e865374dec34bc0ebb9a14d59a2573bd18c3
Justin <atomyuangao@gmail.com>
2024-01-09 21:40:24 +0800

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = '0.1.9'
+VERSION = '0.1.10'
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
